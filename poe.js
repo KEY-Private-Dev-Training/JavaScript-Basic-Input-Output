48d1865ec88d5ebd29be4dbbc6e2eca8a48283a6
Kevin Yancy <kyancy@gmail.com>
2022-08-08 16:33:44 -0500

    Added starter code

---
+++ poe.js
@@ -0,0 +1,10 @@
+/*
+
+Put all of your answer code here. 
+(note we can do multiline C/Java style comments in JS too if you want)
+
+*/
+
+// Make sure this JS file actually gets included with HTML
+console,console.log("JS Loaded!");
+
